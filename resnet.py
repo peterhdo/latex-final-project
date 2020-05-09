f3e2b2f6e30338069651ccac868383bf9a2f20e0
Peter Do <peter@peterhdo.com>
2020-05-08 18:34:13 -0700

    resnet

---
+++ resnet.py
@@ -0,0 +1,121 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def evaluate(model, device, test_loader, val=True):
+    if val:
+        type = "Dev"
+    else:
+        type = "Test"
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\n{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        type, test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=1024, metavar='N',
+                        help='input batch size for training')
+    parser.add_argument('--epochs', type=int, default=5, metavar='N',
+                        help='number of epochs to train')
+    parser.add_argument('--lr', type=float, default=1e-2, metavar='LR',
+                        help='learning rate')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder('./datasets/train/',
+                             transform=transforms.Compose([
+                                 transforms.Grayscale(num_output_channels=3),
+                                 transforms.CenterCrop((256,256)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                             ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    dev_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder('./datasets/dev/',
+                             transform=transforms.Compose([
+                                 transforms.Grayscale(num_output_channels=3),
+                                 transforms.CenterCrop((256,256)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                             ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder('./datasets/test/',
+                             transform=transforms.Compose([
+                                 transforms.Grayscale(num_output_channels=3),
+                                 transforms.CenterCrop((256,256)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                             ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+
+    model = torch.hub.load('pytorch/vision:v0.4.2', 'resnet50', pretrained=False).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        evaluate(model, device, dev_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "latex_cnn.pt")
+
+    evaluate(model, device, test_loader)
+
+
+if __name__ == '__main__':
+    main()
