c0e3878f0eac30953f189100eb503885c0c57a55
Peter Do <peter@peterhdo.com>
2020-05-25 13:05:08 -0700

    top5 accuracy, resnet152, original dataset

--- resnet.py
+++ resnet.py
@@ -9,6 +9,48 @@ from torch.optim.lr_scheduler import StepLR
 
 ce_loss = torch.nn.CrossEntropyLoss(size_average=False)
 
+
+# https://github.com/pytorch/examples/blob/d91adc972cef0083231d22bcc75b7aaa30961863/imagenet/main.py#L407
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+# https://github.com/pytorch/examples/blob/d91adc972cef0083231d22bcc75b7aaa30961863/imagenet/main.py#L359
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
 def train(args, model, device, train_loader, optimizer, epoch, dev_loader):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -30,6 +72,7 @@ def evaluate(model, device, test_loader, type="Dev"):
     model.eval()
     test_loss = 0
     correct = 0
+    top5 = AverageMeter('Acc@5', ':6.2f')
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
@@ -38,16 +81,25 @@ def evaluate(model, device, test_loader, type="Dev"):
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
+            acc5 = accuracy(output, target, topk=(1,5))[1]
+            top5.update(acc5[0], data.size(0))
+
     test_loss /= len(test_loader.dataset)
 
     print('\n{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         type, test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+    import pdb
+    pdb.set_trace()
+
+    print(' * Acc@5 {top5.avg:.3f}'
+          .format(top5=top5))
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch ResNet')
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
                         help='input batch size for training')
     parser.add_argument('--epochs', type=int, default=5, metavar='N',
                         help='number of epochs to train')
@@ -74,7 +126,7 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
     train_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder('./datasets50/train/',
+        datasets.ImageFolder('./datasets/train/',
                              transform=transforms.Compose([
                                  transforms.Grayscale(num_output_channels=3),
                                  transforms.Resize((256,256)),
@@ -83,7 +135,7 @@ def main():
                              ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     dev_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder('./datasets50/dev/',
+        datasets.ImageFolder('./datasets/dev/',
                              transform=transforms.Compose([
                                  transforms.Grayscale(num_output_channels=3),
                                  transforms.Resize((256,256)),
@@ -92,7 +144,7 @@ def main():
                              ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder('./datasets50/test/',
+        datasets.ImageFolder('./datasets/test/',
                              transform=transforms.Compose([
                                  transforms.Grayscale(num_output_channels=3),
                                  transforms.Resize((256,256)),
@@ -101,7 +153,7 @@ def main():
                              ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    model = torch.hub.load('pytorch/vision:v0.4.2', 'resnet50', pretrained=False).to(device)
+    model = torch.hub.load('pytorch/vision:v0.4.2', 'resnet152', pretrained=False).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     for epoch in range(1, args.epochs + 1):
