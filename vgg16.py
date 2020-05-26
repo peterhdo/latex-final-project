b1c4324325634aa032d29015ed638cc9d1e574b4
Kevin Baichoo <kbaichoo@stanford.edu>
2020-05-26 13:36:13 -0700

    Cleaned up vgg16.py.

--- vgg16.py
+++ vgg16.py
@@ -2,16 +2,15 @@ from __future__ import print_function
 import argparse
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
 
 ce_loss = torch.nn.CrossEntropyLoss(size_average=False)
 
 # Constants to change if we changing the number of classes, dataset path, etc.
 DATASET_BASE_PATH = './datasets50'
 NUM_OUTPUT_CLASSES = 50
+
 
 def train(args, model, device, train_loader, optimizer, epoch, dev_loader):
     model.train()
@@ -39,7 +38,8 @@ def evaluate(model, device, test_loader, type="Dev"):
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += ce_loss(output, target)  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
@@ -48,10 +48,11 @@ def evaluate(model, device, test_loader, type="Dev"):
         type, test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch ResNet')
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training')
     parser.add_argument('--epochs', type=int, default=5, metavar='N',
                         help='number of epochs to train')
@@ -62,9 +63,12 @@ def main():
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--log-eval-interval', type=int, default=100, metavar='N',
-                        help='how many batches to wait before logging dev status during training')
+                        help='how many batches to wait before logging '
+                        'training status')
+    parser.add_argument('--log-eval-interval', type=int, default=100,
+                        metavar='N',
+                        help='how many batches to wait before logging dev '
+                        'status during training')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
 
@@ -76,50 +80,54 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
     print('Using gpu: {}'.format(use_cuda))
 
-    # TODO(kbaichoo): use more num_workers -- likely have memory on machines
-    # to raise it up for 2,4... might make mini-batch processing better
+    # You can use more num_workers if there's more memory on the machine
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    
+
     train_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder('{}/train/'.format(DATASET_BASE_PATH),
                              transform=transforms.Compose([
                                  transforms.Grayscale(num_output_channels=3),
-                                 transforms.Resize((224,224)),
+                                 transforms.Resize((224, 224)),
                                  transforms.ToTensor(),
-                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                                 transforms.Normalize(
+                                     mean=[0.485, 0.456, 0.406], std=[
+                                         0.229, 0.224, 0.225])
                              ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     dev_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder('{}/dev/'.format(DATASET_BASE_PATH),
                              transform=transforms.Compose([
                                  transforms.Grayscale(num_output_channels=3),
-                                 transforms.Resize((224,224)),
+                                 transforms.Resize((224, 224)),
                                  transforms.ToTensor(),
-                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                                 transforms.Normalize(
+                                     mean=[0.485, 0.456, 0.406], std=[
+                                         0.229, 0.224, 0.225])
                              ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder('{}/test/'.format(DATASET_BASE_PATH),
                              transform=transforms.Compose([
                                  transforms.Grayscale(num_output_channels=3),
-                                 transforms.Resize((224,224)),
+                                 transforms.Resize((224, 224)),
                                  transforms.ToTensor(),
-                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                                 transforms.Normalize(
+                                     mean=[0.485, 0.456, 0.406], std=[
+                                         0.229, 0.224, 0.225])
                              ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    # TODO(kbaichoo): modify the model (so it's vgg + vgg we want)
-    model = torch.hub.load('pytorch/vision:v0.6.0', 'vgg16_bn', pretrained=True).to(device)
-    
+    model = torch.hub.load('pytorch/vision:v0.6.0',
+                           'vgg16_bn', pretrained=True).to(device)
+
     # Freeze training for all layers (since we're just appending a final layer)
     for param in model.features.parameters():
         param.require_grad = False
 
     # Newly created modules have require_grad=True by default
-    #num_features = model.classifier[6].in_features
     num_features = model.classifier[-1].in_features
     features = list(model.classifier.children())[:-1]  # Remove last layer
-    # Add our layer with NUM_OUTPUT_CLASSES 
+    # Add our layer with NUM_OUTPUT_CLASSES
     features.extend([nn.Linear(num_features, NUM_OUTPUT_CLASSES)])
     features[-1].to(device)
     model.classifier = nn.Sequential(*features)  # Replace the model classifier
